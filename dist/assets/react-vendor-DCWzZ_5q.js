17ee21a6f40ea696b02090bd523070126cd2ab4c
elibluebird0002 <ellybarikinkya@gmail.com>
2026-02-28 17:59:41 +0300

    prototype design

---
+++ dist/assets/react-vendor-DCWzZ_5q.js
@@ -0,0 +1,1 @@
+import"./leaflet-vendor-uc4iAdmn.js";import"./lucide-vendor-Cdhxaw9g.js";
--- vite.config.js
+++ vite.config.js
@@ -3,4 +3,19 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
+  build: {
+    rollupOptions: {
+      output: {
+        manualChunks: {
+          'react-vendor': ['react', 'react-dom'],
+          'leaflet-vendor': ['leaflet', 'react-leaflet'],
+          'lucide-vendor': ['lucide-react']
+        }
+      }
+    },
+    chunkSizeWarningLimit: 1000
+  },
+  optimizeDeps: {
+    include: ['react', 'react-dom', 'leaflet', 'react-leaflet', 'lucide-react']
+  }
 })
